67f097bfc1e9fb1702a425c720ec08bf98688663
Nate Parsons <parsons.nate@gmail.com>
2010-04-19 00:55:49 -0400

    Adding source for lombok
    Adding testng for unit tests
    
    Adding MathUtils because Java won't allow anonymous functions

---
+++ src/jskills/numerics/MathUtils.java
@@ -0,0 +1,13 @@
+package jskills.numerics;
+
+/**
+ * For all the functions that aren't in java.lang.Math
+ */
+public final class MathUtils {
+
+	/** Don't allow instantiation **/
+	private MathUtils() { }
+	
+	/** Square a number **/
+	public static double square(double x) { return x*x; }
+}
