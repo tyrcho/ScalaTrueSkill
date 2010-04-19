34dc39f13bbbd6ec11ab64914712f901b1407f2c
Nate Parsons <parsons.nate@gmail.com>
2010-04-19 13:04:08 -0400

    Converted Range.java. Until I figure out how to make Java's generics more like C#, there will be only Range.java for all of your Range-ing needs.

--- src/jskills/numerics/Range.java
+++ src/jskills/numerics/Range.java
@@ -1,49 +1,50 @@
-﻿using System;
+﻿package jskills.numerics;
 
-namespace Moserware.Skills.Numerics
-{
-    // The whole purpose of this class is to make the code for the SkillCalculator(s)
-    // look a little cleaner
+import lombok.Data;
 
-    public abstract class Range<T> where T : Range<T>, new()
-    {
-        private static readonly T _Instance = new T();
+/**
+ * A very limited implementation of an immutable range of integers, including
+ * endpoints. There is no such thing as an empty range.
+ * <p>
+ * The whole purpose of this class is to make the code for the
+ * SkillCalculator(s) look a little cleaner
+ * <p>
+ * Derived classes can't use this class's static ctors they way they could in
+ * C#, so I'm going to eschew the relative type safety afforded by Moser's
+ * scheme and make this class final. A Range is a Range is a Range.
+ */
+@Data public final class Range { 
 
-        protected Range(int min, int max)
-        {
-            if (min > max)
-            {
-                throw new ArgumentOutOfRangeException();
-            }
+	private final int min;
+	private final int max;
 
-            Min = min;
-            Max = max;
-        }
+	public Range(int min, int max) {
+        if (min > max) throw new IllegalArgumentException();
 
-        public int Min { get; private set; }
-        public int Max { get; private set; }
-        protected abstract T Create(int min, int max);
+        this.min = min;
+        this.max = max;
+    }
 
-        // REVIEW: It's probably bad form to have access statics via a derived class, but the syntax looks better :-)
+    public boolean isInRange(int value) {
+        return (min <= value) && (value <= max);
+    }
 
-        public static T Inclusive(int min, int max)
-        {
-            return _Instance.Create(min, max);
-        }
+    // REVIEW: It's probably bad form to have access statics via a derived
+    // class, but the syntax looks better :-)
 
-        public static T Exactly(int value)
-        {
-            return _Instance.Create(value, value);
-        }
+    // It's bad form in Java to the point where it won't compile. Using statics
+    // through derived classes gets you a warning, but accessing generic types
+    // (T) won't compile.  
 
-        public static T AtLeast(int minimumValue)
-        {
-            return _Instance.Create(minimumValue, int.MaxValue);
-        }
+    public static Range inclusive(int min, int max) {
+        return new Range(min, max);
+    }
 
-        public bool IsInRange(int value)
-        {
-            return (Min <= value) && (value <= Max);
-        }
+    public static Range exactly(int value) {
+        return new Range(value, value);
+    }
+
+    public static Range atLeast(int minimumValue) {
+        return new Range(minimumValue, Integer.MAX_VALUE);
     }
 }
