d6fc89b03d9f06ff554244940d00acfb4ca9639e
Nate Parsons <parsons.nate@gmail.com>
2010-04-20 22:23:15 -0400

    ISupportPartialPlay converted.
    Changed property->getter

--- src/jskills/ISupportPartialPlay.java
+++ src/jskills/ISupportPartialPlay.java
@@ -1,13 +1,14 @@
-﻿namespace Moserware.Skills
-{
-    /// <summary>
-    /// Indicates support for allowing partial play (where a player only plays a part of the time).
-    /// </summary>
-    public interface ISupportPartialPlay
-    {
-        /// <summary>
-        /// Indicates the percent of the time the player should be weighted where 0.0 indicates the player didn't play and 1.0 indicates the player played 100% of the time.
-        /// </summary>        
-        double PartialPlayPercentage { get; }
-    }
+﻿package jskills;
+
+/**
+ * Indicates support for allowing partial play (where a player only plays a part
+ * of the time).
+ */
+public interface ISupportPartialPlay {
+    /**
+     * Indicates the percent of the time the player should be weighted where 0.0
+     * indicates the player didn't play and 1.0 indicates the player played 100%
+     * of the time.
+     */
+    public double getPartialPlayPercentage();
 }
