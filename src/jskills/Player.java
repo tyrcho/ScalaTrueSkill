af73505f0fde2154483db0e249cf37d2f4a9fb71
Nate Parsons <parsons.nate@gmail.com>
2010-04-21 00:51:19 -0400

    Converted Player * implements interfaces * @Getter instead of properties * No more convenience player

--- src/jskills/Player.java
+++ src/jskills/Player.java
@@ -1,129 +1,83 @@
-﻿namespace Moserware.Skills
-{
-    /// <summary>
-    /// Represents a player who has a <see cref="Rating"/>.
-    /// </summary>
-    public class Player<T> : ISupportPartialPlay, ISupportPartialUpdate
-    {
-        private final double DefaultPartialPlayPercentage = 1.0; // = 100% play time
-        private final double DefaultPartialUpdatePercentage = 1.0; // = receive 100% update
-        private readonly T _Id;
-        private readonly double _PartialPlayPercentage;
+﻿package jskills;
 
-        private readonly double _PartialUpdatePercentage;
+import lombok.Getter;
 
-        /// <summary>
-        /// Constructs a player.
-        /// </summary>
-        /// <param name="id">The identifier for the player, such as a name.</param>
-        public Player(T id)
-            : this(id, DefaultPartialPlayPercentage, DefaultPartialUpdatePercentage)
-        {
-        }
+/**
+ * Represents a player who has a {@link Rating}.
+ */
+public class Player<T> implements ISupportPartialPlay, ISupportPartialUpdate {
 
-        /// <summary>
-        /// Constructs a player.
-        /// </summary>
-        /// <param name="id">The identifier for the player, such as a name.</param>
-        /// <param name="partialPlayPercentage">The weight percentage to give this player when calculating a new rank.</param>        
-        public Player(T id, double partialPlayPercentage)
-            : this(id, partialPlayPercentage, DefaultPartialUpdatePercentage)
-        {
-        }
+    /** = 100% play time **/
+    private static final double DefaultPartialPlayPercentage = 1.0;
 
-        /// <summary>
-        /// Constructs a player.
-        /// </summary>
-        /// <param name="id">The identifier for the player, such as a name.</param>
-        /// <param name="partialPlayPercentage">The weight percentage to give this player when calculating a new rank.</param>
-        /// <param name="partialUpdatePercentage">/// Indicated how much of a skill update a player should receive where 0 represents no update and 1.0 represents 100% of the update.</param>
-        public Player(T id, double partialPlayPercentage, double partialUpdatePercentage)
-        {
-            // If they don't want to give a player an id, that's ok...
-            Guard.ArgumentInRangeInclusive(partialPlayPercentage, 0, 1.0, "partialPlayPercentage");
-            Guard.ArgumentInRangeInclusive(partialUpdatePercentage, 0, 1.0, "partialUpdatePercentage");
-            _Id = id;
-            _PartialPlayPercentage = partialPlayPercentage;
-            _PartialUpdatePercentage = partialUpdatePercentage;
-        }
+    /** = receive 100% update **/
+    private static final double DefaultPartialUpdatePercentage = 1.0;
 
-        /// <summary>
-        /// The identifier for the player, such as a name.
-        /// </summary>
-        public T Id
-        {
-            get { return _Id; }
-        }
+    /** The identifier for the player, such as a name. **/
+    @Getter private final T id;
 
-        #region ISupportPartialPlay Members
+    /**
+     * Indicates the percent of the time the player should be weighted where 0.0
+     * indicates the player didn't play and 1.0 indicates the player played 100%
+     * of the time.
+     */
+    @Getter private final double partialPlayPercentage;
 
-        /// <summary>
-        /// Indicates the percent of the time the player should be weighted where 0.0 indicates the player didn't play and 1.0 indicates the player played 100% of the time.
-        /// </summary>        
-        public double PartialPlayPercentage
-        {
-            get { return _PartialPlayPercentage; }
-        }
+    /**
+     * Indicated how much of a skill update a player should receive where 0.0
+     * represents no update and 1.0 represents 100% of the update.
+     */
+    @Getter private final double partialUpdatePercentage;
 
-        #endregion
-
-        #region ISupportPartialUpdate Members
-
-        /// <summary>
-        /// Indicated how much of a skill update a player should receive where 0.0 represents no update and 1.0 represents 100% of the update.
-        /// </summary>
-        public double PartialUpdatePercentage
-        {
-            get { return _PartialUpdatePercentage; }
-        }
-
-        #endregion
-
-        public override string ToString()
-        {
-            if (Id != null)
-            {
-                return Id.ToString();
-            }
-
-            return base.ToString();
-        }
+    /**
+     * Constructs a player.
+     * 
+     * @param id
+     *            The identifier for the player, such as a name.
+     */
+    public Player(T id) {
+        this(id, DefaultPartialPlayPercentage, DefaultPartialUpdatePercentage);
     }
 
-    /// <summary>
-    /// Represents a player who has a <see cref="Rating"/>.
-    /// </summary>
-    public class Player : Player<object>
-    {
-        /// <summary>
-        /// Constructs a player.
-        /// </summary>
-        /// <param name="id">The identifier for the player, such as a name.</param>
-        public Player(object id)
-            : base(id)
-        {
-        }
+    /**
+     * Constructs a player.
+     * 
+     * @param id
+     *            The identifier for the player, such as a name.
+     * @param partialPlayPercentage
+     *            The weight percentage to give this player when calculating a
+     *            new rank.
+     */
+    public Player(T id, double partialPlayPercentage) {
+        this(id, partialPlayPercentage, DefaultPartialUpdatePercentage);
+    }
 
-        /// <summary>
-        /// Constructs a player.
-        /// </summary>
-        /// <param name="id">The identifier for the player, such as a name.</param>
-        /// <param name="partialPlayPercentage">The weight percentage to give this player when calculating a new rank.</param>
-        /// <param name="partialUpdatePercentage">Indicated how much of a skill update a player should receive where 0 represents no update and 1.0 represents 100% of the update.</param>
-        public Player(object id, double partialPlayPercentage)
-            : base(id, partialPlayPercentage)
-        {
-        }
+    /**
+     * Constructs a player.
+     * 
+     * @param id
+     *            The identifier for the player, such as a name.
+     * @param partialPlayPercentage
+     *            The weight percentage to give this player when calculating a
+     *            new rank.
+     * @param partialUpdatePercentage
+     *            Indicates how much of a skill update a player should receive
+     *            where 0 represents no update and 1.0 represents 100% of the
+     *            update.
+     */
+    public Player(T id, double partialPlayPercentage,
+            double partialUpdatePercentage) {
+        // If they don't want to give a player an id, that's ok...
+        Guard.ArgumentInRangeInclusive(partialPlayPercentage, 0, 1.0,
+                "partialPlayPercentage");
+        Guard.ArgumentInRangeInclusive(partialUpdatePercentage, 0, 1.0,
+                "partialUpdatePercentage");
+        this.id = id;
+        this.partialPlayPercentage = partialPlayPercentage;
+        this.partialUpdatePercentage = partialUpdatePercentage;
+    }
 
-        /// <summary>
-        /// Constructs a player.
-        /// </summary>
-        /// <param name="id">The identifier for the player, such as a name.</param>
-        /// <param name="partialPlayPercentage">The weight percentage to give this player when calculating a new rank.</param>
-        /// <param name="partialUpdatePercentage">Indicated how much of a skill update a player should receive where 0 represents no update and 1.0 represents 100% of the update.</param>
-        public Player(object id, double partialPlayPercentage, double partialUpdatePercentage)
-            : base(id, partialPlayPercentage, partialUpdatePercentage)
-        {
-        }
+    @Override public String toString() {
+        return id != null ? id.toString() : super.toString();
     }
 }
