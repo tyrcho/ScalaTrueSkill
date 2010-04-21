1b2b440cac6686a379599d95dd301c8ce4ed28d3
Nate Parsons <parsons.nate@gmail.com>
2010-04-20 22:33:32 -0400

    PairwiseComparison converted * Java enums need constructor and explicit field.

--- src/jskills/PairwiseComparison.java
+++ src/jskills/PairwiseComparison.java
@@ -1,15 +1,17 @@
-﻿namespace Moserware.Skills
-{
-    /// <summary>
-    /// Represents a comparison between two players.
-    /// </summary>
-    /// <remarks>
-    /// The actual values for the enum were chosen so that the also correspond to the multiplier for updates to means.
-    /// </remarks>
-    public enum PairwiseComparison
-    {
-        Win = 1,
-        Draw = 0,
-        Lose = -1
-    }
+﻿package jskills;
+
+/**
+ * Represents a comparison between two players.
+ * <p>
+ * The actual values for the enum were chosen so that the also correspond to the
+ * multiplier for updates to means.
+ */
+public enum PairwiseComparison{
+    WIN(1),
+    DRAW(0),
+    LOSE(-1);
+    
+    public final int multiplier;
+    
+    private PairwiseComparison(int multiplier) { this.multiplier = multiplier; }
 }
