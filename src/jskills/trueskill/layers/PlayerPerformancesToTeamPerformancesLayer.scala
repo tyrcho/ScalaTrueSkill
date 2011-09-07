02acb37c07bc5577dd8612f1a5e99129f3dbaf29
Michel Daviot <tyrcho@tyrcho.com>
2011-09-07 22:38:51 +0200

    java2scala some more classes + removed dead classes

---
+++ src/jskills/trueskill/layers/PlayerPerformancesToTeamPerformancesLayer.scala
@@ -0,0 +1,73 @@
+package jskills.trueskill.layers;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import jskills.IPlayer;
+import jskills.PartialPlay;
+import jskills.factorgraphs.KeyedVariable;
+import jskills.factorgraphs.Schedule;
+import jskills.factorgraphs.ScheduleStep;
+import jskills.factorgraphs.Variable;
+import jskills.numerics.GaussianDistribution;
+import jskills.trueskill.TrueSkillFactorGraph;
+import jskills.trueskill.factors.GaussianWeightedSumFactor;
+import collection.JavaConversions._
+
+class PlayerPerformancesToTeamPerformancesLayer(parentGraph: TrueSkillFactorGraph)
+  extends TrueSkillFactorGraphLayer[KeyedVariable[IPlayer, GaussianDistribution], GaussianWeightedSumFactor, Variable[GaussianDistribution]](parentGraph) {
+
+  override def BuildLayer() {
+    for (currentTeam <- getInputVariablesGroups()) {
+      val teamPerformance = CreateOutputVariable(currentTeam);
+      AddLayerFactor(createPlayerToTeamSumFactor(currentTeam,
+        teamPerformance));
+
+      // REVIEW: Does it make sense to have groups of one?
+      addOutputVariable(teamPerformance);
+    }
+  }
+
+  override def createPriorSchedule(): Schedule[GaussianDistribution] = {
+    val schedules = new ArrayList[Schedule[GaussianDistribution]]();
+    for (weightedSumFactor <- getLocalFactors()) {
+      schedules.add(new ScheduleStep[GaussianDistribution](
+        "Perf to Team Perf Step", weightedSumFactor, 0));
+    }
+    return ScheduleSequence(schedules,
+      "all player perf to team perf schedule");
+  }
+
+  def createPlayerToTeamSumFactor(teamMembers: List[KeyedVariable[IPlayer, GaussianDistribution]], sumVariable: Variable[GaussianDistribution]): GaussianWeightedSumFactor = {
+    val weights = new Array[Double](teamMembers.size());
+    for (i <- 0 until weights.length) {
+      weights(i) = PartialPlay.getPartialPlayPercentage(teamMembers
+        .get(i).getKey());
+    }
+    return new GaussianWeightedSumFactor(sumVariable, teamMembers, weights);
+  }
+
+  override def createPosteriorSchedule(): Schedule[GaussianDistribution] = {
+    val schedules = new ArrayList[Schedule[GaussianDistribution]]();
+    for (currentFactor <- getLocalFactors()) {
+      // TODO is there an off by 1 error here?
+      for (i <- 0 until currentFactor.getNumberOfMessages()) {
+        schedules.add(new ScheduleStep[GaussianDistribution](
+          "team sum perf @" + i, currentFactor, i));
+      }
+    }
+    return ScheduleSequence(schedules, "all of the team's sum iterations");
+  }
+
+  private def CreateOutputVariable(team: List[KeyedVariable[IPlayer, GaussianDistribution]]): Variable[GaussianDistribution] = {
+    val sb = new StringBuilder();
+    for (teamMember <- team) {
+      sb.append(teamMember.getKey().toString());
+      sb.append(", ");
+    }
+    sb.delete(sb.length() - 2, sb.length());
+
+    return new Variable[GaussianDistribution](GaussianDistribution.UNIFORM, "Team[%s]'s performance", sb.toString());
+  }
+}
---
+++ src/jskills/trueskill/layers/PlayerPriorValuesToSkillsLayer.scala
@@ -0,0 +1,60 @@
+package jskills.trueskill.layers;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map.Entry;
+
+import jskills.IPlayer;
+import jskills.ITeam;
+import jskills.Rating;
+import jskills.factorgraphs.DefaultVariable;
+import jskills.factorgraphs.KeyedVariable;
+import jskills.factorgraphs.Schedule;
+import jskills.factorgraphs.ScheduleStep;
+import jskills.factorgraphs.Variable;
+import jskills.numerics.GaussianDistribution;
+import jskills.numerics.MathUtils;
+import jskills.trueskill.TrueSkillFactorGraph;
+import jskills.trueskill.factors.GaussianPriorFactor;
+import collection.JavaConversions._
+
+// We intentionally have no Posterior schedule since the only purpose here is to 
+class PlayerPriorValuesToSkillsLayer(parentGraph: TrueSkillFactorGraph, teams: Collection[ITeam])
+  extends TrueSkillFactorGraphLayer[DefaultVariable[GaussianDistribution], GaussianPriorFactor, KeyedVariable[IPlayer, GaussianDistribution]](parentGraph) {
+
+  override def BuildLayer() {
+    for (currentTeam <- teams) {
+      val currentTeamSkills = new ArrayList[KeyedVariable[IPlayer, GaussianDistribution]]();
+
+      for (
+        currentTeamPlayer <- currentTeam
+          .entrySet()
+      ) {
+        val playerSkill = CreateSkillOutputVariable(currentTeamPlayer.getKey());
+        AddLayerFactor(CreatePriorFactor(currentTeamPlayer.getKey(),
+          currentTeamPlayer.getValue(), playerSkill));
+        currentTeamSkills.add(playerSkill);
+      }
+
+      addOutputVariableGroup(currentTeamSkills);
+    }
+  }
+
+  override def createPriorSchedule(): Schedule[GaussianDistribution] = {
+    val schedules = new ArrayList[Schedule[GaussianDistribution]]();
+    for (prior <- getLocalFactors()) {
+      schedules.add(new ScheduleStep[GaussianDistribution](
+        "Prior to Skill Step", prior, 0));
+    }
+    return ScheduleSequence(schedules, "All priors");
+  }
+
+  private def CreatePriorFactor(player: IPlayer, priorRating: Rating, skillsVariable: Variable[GaussianDistribution]): GaussianPriorFactor =
+    new GaussianPriorFactor(priorRating.getMean(),
+      MathUtils.square(priorRating.getStandardDeviation()) + MathUtils.square(getParentFactorGraph().getGameInfo().getDynamicsFactor()),
+      skillsVariable)
+
+  private def CreateSkillOutputVariable(key: IPlayer): KeyedVariable[IPlayer, GaussianDistribution] =
+    new KeyedVariable[IPlayer, GaussianDistribution](key, GaussianDistribution.UNIFORM, "%s's skill", key.toString());
+}
