edc74a2d3805e1ba1d61ff5741832bbf2d4e981c
Michel Daviot <michel@daviot.info>
2011-09-12 22:27:18 +0200

    DefaultVariable.scala

---
+++ src/jskills/factorgraphs/DefaultVariable.scala
@@ -0,0 +1,7 @@
+package jskills.factorgraphs;
+
+class DefaultVariable[TValue] extends Variable[TValue](null.asInstanceOf[TValue], "Default") {
+  override def setValue(value: TValue) {
+    throw new UnsupportedOperationException();
+  }
+}
