d6465f30945f170ca94d7d4842e6e57b6f6508dc
Michel Daviot <michel@daviot.info>
2011-09-12 22:59:29 +0200

    fixed TrueSkillFactorGraph.scala

--- src/jskills/trueskill/TrueSkillFactorGraph.scala
+++ src/jskills/trueskill/TrueSkillFactorGraph.scala
@@ -1,135 +1,107 @@
 package jskills.trueskill;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import jskills.GameInfo;
-import jskills.IPlayer;
-import jskills.ITeam;
-import jskills.Rating;
-import jskills.factorgraphs.Factor;
-import jskills.factorgraphs.FactorGraph;
-import jskills.factorgraphs.FactorGraphLayerBase;
-import jskills.factorgraphs.FactorList;
-import jskills.factorgraphs.KeyedVariable;
-import jskills.factorgraphs.Schedule;
-import jskills.factorgraphs.ScheduleSequence;
-import jskills.numerics.GaussianDistribution;
-import jskills.trueskill.layers.IteratedTeamDifferencesInnerLayer;
-import jskills.trueskill.layers.PlayerPerformancesToTeamPerformancesLayer;
-import jskills.trueskill.layers.PlayerPriorValuesToSkillsLayer;
-import jskills.trueskill.layers.PlayerSkillsToPerformancesLayer;
-import jskills.trueskill.layers.TeamDifferencesComparisonLayer;
+import java.util.ArrayList
+import java.util.Collection
+import java.util.Collections
+import java.util.HashMap
+import java.util.List
+import java.util.Map
+import jskills.GameInfo
+import jskills.IPlayer
+import jskills.ITeam
+import jskills.Rating
+import jskills.factorgraphs.Factor
+import jskills.factorgraphs.FactorGraph
+import jskills.factorgraphs.FactorGraphLayerBase
+import jskills.factorgraphs.FactorList
+import jskills.factorgraphs.KeyedVariable
+import jskills.factorgraphs.Schedule
+import jskills.factorgraphs.ScheduleSequence
+import jskills.numerics.GaussianDistribution
+import jskills.trueskill.layers.IteratedTeamDifferencesInnerLayer
+import jskills.trueskill.layers.PlayerPerformancesToTeamPerformancesLayer
+import jskills.trueskill.layers.PlayerPriorValuesToSkillsLayer
+import jskills.trueskill.layers.PlayerSkillsToPerformancesLayer
+import jskills.trueskill.layers.TeamDifferencesComparisonLayer
 import jskills.trueskill.layers.TeamPerformancesToTeamPerformanceDifferencesLayer;
+import scala.reflect.BeanProperty
+import collection.JavaConversions._
 
-public class TrueSkillFactorGraph extends FactorGraph<TrueSkillFactorGraph>
-{
-    private final List<FactorGraphLayerBase<GaussianDistribution>> _Layers = new ArrayList<FactorGraphLayerBase<GaussianDistribution>>();
-    private final PlayerPriorValuesToSkillsLayer _PriorLayer;
+class TrueSkillFactorGraph(
+  @BeanProperty val gameInfo: GameInfo, teams: Collection[ITeam], teamRanks: Array[Int])
+  extends FactorGraph[TrueSkillFactorGraph] {
+  val _Layers = new ArrayList[FactorGraphLayerBase[GaussianDistribution]]();
+  val _PriorLayer = new PlayerPriorValuesToSkillsLayer(this, teams);
 
-    public TrueSkillFactorGraph(GameInfo gameInfo, Collection<ITeam> teams, int[] teamRanks)
-    {
-        _PriorLayer = new PlayerPriorValuesToSkillsLayer(this, teams);
-        setGameInfo(gameInfo);
+  _Layers.add(_PriorLayer);
+  _Layers.add(new PlayerSkillsToPerformancesLayer(this));
+  _Layers.add(new PlayerPerformancesToTeamPerformancesLayer(this));
+  _Layers.add(new IteratedTeamDifferencesInnerLayer(
+    this,
+    new TeamPerformancesToTeamPerformanceDifferencesLayer(this),
+    new TeamDifferencesComparisonLayer(this, teamRanks)));
 
-        _Layers.add(_PriorLayer);
-        _Layers.add(new PlayerSkillsToPerformancesLayer(this));
-        _Layers.add(new PlayerPerformancesToTeamPerformancesLayer(this));
-        _Layers.add(new IteratedTeamDifferencesInnerLayer(
-                              this,
-                              new TeamPerformancesToTeamPerformanceDifferencesLayer(this),
-                              new TeamDifferencesComparisonLayer(this, teamRanks)));
+  def BuildGraph() {
+    var lastOutput: Any = null
+    for (currentLayer <- _Layers) {
+      if (lastOutput != null) {
+        currentLayer.SetRawInputVariablesGroups(lastOutput);
+      }
+      currentLayer.BuildLayer();
+      lastOutput = currentLayer.GetRawOutputVariablesGroups();
+    }
+  }
+
+  def RunSchedule() {
+    val fullSchedule = CreateFullSchedule();
+    fullSchedule.visit();
+  }
+
+  def GetProbabilityOfRanking(): Double = {
+    val factorList = new FactorList[GaussianDistribution]();
+
+    for (currentLayer <- _Layers) {
+      for (currentFactor <- currentLayer.getUntypedFactors()) {
+        factorList.addFactor(currentFactor);
+      }
     }
 
-    private GameInfo gameInfo;
-    public GameInfo getGameInfo() { return gameInfo; }
-    private void setGameInfo(GameInfo info) { gameInfo = info; } 
+    val logZ = factorList.getLogNormalization();
+    return Math.exp(logZ);
+  }
 
-    public void BuildGraph()
-    {
-        Object lastOutput = null;
+  private def CreateFullSchedule(): Schedule[GaussianDistribution] = {
+    val fullSchedule = new ArrayList[Schedule[GaussianDistribution]]();
 
-        for(FactorGraphLayerBase<GaussianDistribution> currentLayer :_Layers)
-        {
-            if (lastOutput != null)
-            {
-                currentLayer.SetRawInputVariablesGroups(lastOutput);
-            }
-
-            currentLayer.BuildLayer();
-
-            lastOutput = currentLayer.GetRawOutputVariablesGroups();
-        }
+    for (currentLayer <- _Layers) {
+      val currentPriorSchedule = currentLayer.createPriorSchedule();
+      if (currentPriorSchedule != null) {
+        fullSchedule.add(currentPriorSchedule);
+      }
     }
 
-    public void RunSchedule()
-    {
-        Schedule<GaussianDistribution> fullSchedule = CreateFullSchedule();
-        @SuppressWarnings("unused") // TODO Maybe something can be done w/ this?
-        double fullScheduleDelta = fullSchedule.visit();
+    // Getting as a list to use reverse()
+    val allLayers = new ArrayList[FactorGraphLayerBase[GaussianDistribution]](_Layers);
+    Collections.reverse(allLayers);
+
+    for (currentLayer <- allLayers) {
+      val currentPosteriorSchedule = currentLayer.createPosteriorSchedule();
+      if (currentPosteriorSchedule != null) {
+        fullSchedule.add(currentPosteriorSchedule);
+      }
     }
 
-    public double GetProbabilityOfRanking()
-    {
-        FactorList<GaussianDistribution> factorList = new FactorList<GaussianDistribution>();
+    return new ScheduleSequence[GaussianDistribution]("Full schedule", fullSchedule);
+  }
 
-        for(FactorGraphLayerBase<GaussianDistribution> currentLayer :_Layers)
-        {
-            for(Factor<GaussianDistribution> currentFactor :currentLayer.getUntypedFactors())
-            {
-                factorList.addFactor(currentFactor);
-            }
-        }
-
-        double logZ = factorList.getLogNormalization();
-        return Math.exp(logZ);
+  def GetUpdatedRatings(): Map[IPlayer, Rating] = {
+    val result = new HashMap[IPlayer, Rating]();
+    for (currentTeam <- _PriorLayer.getOutputVariablesGroups()) {
+      for (currentPlayer <- currentTeam) {
+        result.put(currentPlayer.getKey(), new Rating(currentPlayer.getValue().getMean(), currentPlayer.getValue().getStandardDeviation()));
+      }
     }
 
-    private Schedule<GaussianDistribution> CreateFullSchedule()
-    {
-        List<Schedule<GaussianDistribution>> fullSchedule = new ArrayList<Schedule<GaussianDistribution>>();
-
-        for(FactorGraphLayerBase<GaussianDistribution> currentLayer :_Layers)
-        {
-            Schedule<GaussianDistribution> currentPriorSchedule = currentLayer.createPriorSchedule();
-            if (currentPriorSchedule != null)
-            {
-                fullSchedule.add(currentPriorSchedule);
-            }
-        }
-
-        // Getting as a list to use reverse()
-        List<FactorGraphLayerBase<GaussianDistribution>> allLayers = new ArrayList<FactorGraphLayerBase<GaussianDistribution>>(_Layers);
-        Collections.reverse(allLayers);
-
-        for(FactorGraphLayerBase<GaussianDistribution> currentLayer : allLayers)
-        {
-            Schedule<GaussianDistribution> currentPosteriorSchedule = currentLayer.createPosteriorSchedule();
-            if (currentPosteriorSchedule != null)
-            {
-                fullSchedule.add(currentPosteriorSchedule);
-            }
-        }
-
-        return new ScheduleSequence<GaussianDistribution>("Full schedule", fullSchedule);
-    }
-
-    public Map<IPlayer, Rating> GetUpdatedRatings()
-    {
-        Map<IPlayer, Rating> result = new HashMap<IPlayer, Rating>();
-        for(List<KeyedVariable<IPlayer, GaussianDistribution>> currentTeam : _PriorLayer.getOutputVariablesGroups())
-        {
-            for(KeyedVariable<IPlayer, GaussianDistribution> currentPlayer : currentTeam)
-            {
-                result.put(currentPlayer.getKey(), new Rating(currentPlayer.getValue().getMean(),
-                                                       currentPlayer.getValue().getStandardDeviation()));
-            }
-        }
-
-        return result;
-    }
+    return result;
+  }
 }
