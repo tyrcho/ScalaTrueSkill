4a29f5b4c20bd26d55695ad032983da0e3f6390c
Michel Daviot <michel@daviot.info>
2011-09-12 19:51:42 +0200

    java2scala FideKFactor

---
+++ src/jskills/elo/FideKFactor.scala
@@ -0,0 +1,13 @@
+package jskills.elo;
+
+/** @see http://ratings.fide.com/calculator_rtd.phtml for details **/
+class FideKFactor(value: Double = -1) extends KFactor(value) {
+  override def getValueForRating(rating: Double): Double = if (rating < 2400) 15 else 10
+}
+
+object FideKFactor {
+  /** Indicates someone who has played less than 30 games. **/
+  class Provisional extends FideKFactor {
+    override def getValueForRating(rating: Double) = 25
+  }
+}
