584c140290eab630e47bca583ddd305761458dcc
Michel Daviot <michel@daviot.info>
2011-09-15 19:26:16 +0200

    Message

---
+++ src/jskills/factorgraphs/Message.scala
@@ -0,0 +1,8 @@
+package jskills.factorgraphs;
+import scala.reflect.BeanProperty
+
+class Message[T](@BeanProperty var value: T, nameFormat: String, args: Object*) {
+  def this() { this(null.asInstanceOf[T], null, null); }
+
+  override def toString() = if (nameFormat == null) super.toString() else format(nameFormat, args)
+}
