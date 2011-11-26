d8e659ee92e4d66af8bb63ca317511c388dcfd30
Michel Daviot <tyrcho@tyrcho.com>
2011-11-26 15:08:18 +0100

    Add Vaadin / appengine natures

---
+++ src/jskills/ui/TrueSkillUI.scala
@@ -0,0 +1,10 @@
+package jskills.ui
+
+import com.vaadin.Application
+import com.vaadin.ui._
+
+class TrueSkillUI extends Application {
+  def init(): Unit = {
+    setMainWindow(new Window("TrueSkill"))
+  }
+}
