f7f87713cb5e1d84f931dea83c4abec00feacc6a
Michel Daviot <michel.daviot@atos.net>
2014-06-11 13:48:33 +0200

    simplifies a bit

--- TrueSkillEngine/src/main/scala/jskills/PairwiseComparison.scala
+++ TrueSkillEngine/src/main/scala/jskills/PairwiseComparison.scala
@@ -6,12 +6,14 @@ package jskills
  * The actual values for the enum were chosen so that the also correspond to the
  * multiplier for updates to means.
  */
-abstract class PairwiseComparison(val multiplier: Int)
+abstract class PairwiseComparison(val multiplier: Int) {
+  def opposite: PairwiseComparison
+}
 
 object PairwiseComparison {
-  case object WIN extends PairwiseComparison(1)
-  case object DRAW extends PairwiseComparison(0)
-  case object LOSE extends PairwiseComparison(-1)
+  case object WIN extends PairwiseComparison(1) { def opposite = LOSE }
+  case object DRAW extends PairwiseComparison(0) { def opposite = DRAW }
+  case object LOSE extends PairwiseComparison(-1) { def opposite = WIN }
 
   def fromMultiplier(multiplier: Int): PairwiseComparison = {
     multiplier match {
--- TrueSkillEngine/src/main/scala/jskills/trueskill/TwoPlayerTrueSkillCalculator.scala
+++ TrueSkillEngine/src/main/scala/jskills/trueskill/TwoPlayerTrueSkillCalculator.scala
@@ -1,8 +1,11 @@
 package jskills.trueskill
 
 import jskills.GameInfo
-import jskills.Player
 import jskills.PairwiseComparison
+import jskills.PairwiseComparison.DRAW
+import jskills.PairwiseComparison.LOSE
+import jskills.PairwiseComparison.WIN
+import jskills.Player
 import jskills.RankSorter
 import jskills.Rating
 import jskills.SkillCalculator
@@ -40,11 +43,19 @@ class TwoPlayerTrueSkillCalculator
 
     Map(winner ->
       calculateNewRating(gameInfo, winnerPreviousRating,
-        loserPreviousRating, if (wasDraw) PairwiseComparison.DRAW else PairwiseComparison.WIN),
+        loserPreviousRating, if (wasDraw) DRAW else WIN),
       loser ->
         calculateNewRating(gameInfo, loserPreviousRating,
-          winnerPreviousRating, if (wasDraw) PairwiseComparison.DRAW else PairwiseComparison.LOSE))
+          winnerPreviousRating, if (wasDraw) DRAW else LOSE))
   }
+
+  def calculateNewRatings(
+    gameInfo: GameInfo,
+    winnerRating: Rating,
+    loserRating: Rating,
+    result: PairwiseComparison): (Rating, Rating) =
+    (calculateNewRating(gameInfo, winnerRating, loserRating, result),
+      calculateNewRating(gameInfo, loserRating, winnerRating, result.opposite))
 
   private def calculateNewRating(gameInfo: GameInfo,
     selfRating: Rating, opponentRating: Rating,
@@ -53,33 +64,25 @@ class TwoPlayerTrueSkillCalculator
 
     val c = Math.sqrt(square(selfRating.standardDeviation) + square(opponentRating.standardDeviation) + 2 * square(gameInfo.beta))
 
-    var winningMean = selfRating.mean
-    var losingMean = opponentRating.mean
-
-    comparison match {
-      case PairwiseComparison.LOSE => {
-        winningMean = opponentRating.mean
-        losingMean = selfRating.mean
-      }
+    val (winningMean, losingMean) = comparison match {
+      case LOSE =>
+        (opponentRating.mean, selfRating.mean)
       case _ =>
+        (selfRating.mean, opponentRating.mean)
     }
 
     val meanDelta = winningMean - losingMean
 
-    var v: Double = 0
-    var w: Double = 0
-    var rankMultiplier: Double = 0
-
-    if (comparison != PairwiseComparison.DRAW) {
-      // non-draw case
-      v = TruncatedGaussianCorrectionFunctions.VExceedsMargin(meanDelta, drawMargin, c)
-      w = TruncatedGaussianCorrectionFunctions.WExceedsMargin(meanDelta, drawMargin, c)
-      rankMultiplier = comparison.multiplier
-    } else {
-      v = TruncatedGaussianCorrectionFunctions.VWithinMargin(meanDelta, drawMargin, c)
-      w = TruncatedGaussianCorrectionFunctions.WWithinMargin(meanDelta, drawMargin, c)
-      rankMultiplier = 1
-    }
+    import TruncatedGaussianCorrectionFunctions._
+    val (v, w, rankMultiplier) =
+      if (comparison != DRAW)
+        (VExceedsMargin(meanDelta, drawMargin, c),
+          WExceedsMargin(meanDelta, drawMargin, c),
+          comparison.multiplier)
+      else
+        (VWithinMargin(meanDelta, drawMargin, c),
+          WWithinMargin(meanDelta, drawMargin, c),
+          1)
 
     val meanMultiplier = (square(selfRating.standardDeviation) + square(gameInfo.dynamicsFactor)) / c
 
@@ -89,7 +92,7 @@ class TwoPlayerTrueSkillCalculator
     val newMean = selfRating.mean + (rankMultiplier * meanMultiplier * v)
     val newStdDev = Math.sqrt(varianceWithDynamics * (1 - w * stdDevMultiplier))
 
-    new Rating(newMean, newStdDev)
+    Rating(newMean, newStdDev)
   }
 
   override def calculateMatchQuality(gameInfo: GameInfo, teams: Seq[Map[Player, Rating]]): Double = {
